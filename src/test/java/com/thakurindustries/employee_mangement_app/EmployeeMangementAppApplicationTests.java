246d113fea72fedfa3e25dbb596b72ecb27412da
Mahendra Singh Thakur <thakurmahendrasingh907@gmail.com>
2025-06-10 22:59:27 +0530

    properties changes

--- src/test/java/com/thakurindustries/employee_mangement_app/EmployeeMangementAppApplicationTests.java
+++ src/test/java/com/thakurindustries/employee_mangement_app/EmployeeMangementAppApplicationTests.java
@@ -1,8 +1,5 @@
 package com.thakurindustries.employee_mangement_app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class EmployeeMangementAppApplicationTests {
 
